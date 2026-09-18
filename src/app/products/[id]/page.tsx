f61d7e9da8060480a25a75171212fe5039d86df6
Md. Rakib Hasan <rakibhasan98840@gmail.com>
2026-09-18 10:58:02 +0600

    not found

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import { notFound } from "next/navigation";
 
-const DynamicPage = () => {
-    return (
-        <div>
-            <h1>Product Details</h1>
-            <p>This is a dynamic product page.</p>
-        </div>
-    );
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+const ProductDetails = async ({ params }: PageProps) => {
+  const { id } = await params;
+
+  if (id !== "1") {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Product Id: {id}</p>
+    </div>
+  );
 };
 
-export default DynamicPage;
+export default ProductDetails;
---
+++ src/app/products/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Product Not Found</h2>
+      <p>Sorry, this product does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
